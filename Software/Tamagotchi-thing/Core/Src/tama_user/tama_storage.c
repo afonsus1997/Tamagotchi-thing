b5f40e9355f329004a6b04a4e14be47876bcb64e
Afonso Muralha <afonsomuralha@gmail.com>
2025-08-02 20:06:28 +0100

    minor changes

---
+++ Software/Tamagotchi-thing/Core/Src/tama_user/tama_storage.c
@@ -0,0 +1,5 @@
+#include "tama_storage.h"
+
+#ifdef  USE_ARRAY_IN_HEADER
+#include "rom_12bit.h"
+#endif  //USE_ARRAY_IN_HEADER
---
+++ Software/Tamagotchi-thing/Core/Src/tama_user/tama_storage.h
@@ -0,0 +1,8 @@
+#ifndef __TAMA_STORAGE__H__
+#define __TAMA_STORAGE__H__
+
+#include "main.h"
+#include "log.h"
+#include "flash.h"
+
+#endif  //!__TAMA_STORAGE__H__
